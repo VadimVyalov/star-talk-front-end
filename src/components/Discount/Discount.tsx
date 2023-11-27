b48ae282e9f5d75623844cda3f5dc03937c81d7c
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-11-27 12:37:11 +0200

    Fix bug 169

--- src/components/Discount/Discount.tsx
+++ src/components/Discount/Discount.tsx
@@ -3,7 +3,7 @@ import cn from "@/helpers"
 import useGetData from "@/hooks/useGetData";
 import Icon from "../Icon";
 import style from "./styles.module.scss";
-//import data from "../../../public/data/discount.json"
+// import data from "../../../public/data/discount.json"
 export type Discount = {
   id: string,
   title: string,
@@ -31,22 +31,22 @@ const Discount = () => {
 
 
           <div className={cn("flex  justify-center",
-            "h-[326px] d:h-[600px]   pt-[76px] d:pt-[196px] pb-6 d:pb-8 overflow-hidden")}>
+            "h-[331px] t:h-[326px] d:h-[600px]  pt-[64px] t:pt-[76px] d:pt-[196px] pb-4 t:pb-6 d:pb-8 overflow-hidden")}>
 
-            <div className="w-[330px] d:w-[540px] px-4 t:px-4 d:px-6 text-white-100  overflow-hidden h-full">
+            <div className="w-[340px] d:w-[540px] px-3 t:px-5 d:px-6 text-white-100  overflow-hidden h-full">
               <h3 className=" flex justify-center flex-wrap gap-x-2 font-roboto font-semibold  text-center text-2xl d:leading-[1.5] d:text-[40px] 
-                     mb-4 d:mb-[60px]"><span >Акція</span> <span >{discount_data.title}</span></h3>
+                     mb-3 t:mb-4 d:mb-[60px]"><span >Акція</span> <span >{discount_data.title}</span></h3>
 
-              <ul className="flex flex-col gap-y-3 t:gap-y-2 d:gap-y-8  ">
+              <ul className="flex flex-col gap-y-2 d:gap-y-8  ">
                 {discount_data.description.map(item => {
                   return (
-                    <li key={item.id} className="flex flex-row text-[14px] d:text-[16px]  leading-[1.25] d:leading-[1.5] ">
+                    <li key={item.id} className="flex flex-row text-[14px] d:text-[16px]  leading-[1.5] t:leading-[1.25] d:leading-[1.5] ">
 
                       <Icon name="/assets/icons/small.svg" id="check"
-                        className={cn(" h-[14px] w-[14px]  t:h-[10px] t:w-[10px]  d:h-[20px] d:w-[20px] shrink-0")}
+                        className={cn(" h-[10px] t:w-[10px]  d:h-[20px] d:w-[20px] shrink-0")}
                       />
 
-                      <p className="ml-2 mb-[1px] t:ml-2 d:ml-3 ">{item.text}</p>
+                      <p className=" mb-[1px] ml-1.5 t:ml-2 d:ml-3 ">{item.text}</p>
                     </li>
                   )
                 })
